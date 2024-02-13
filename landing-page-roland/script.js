246963f0c1c1f51fd5e25dc4935fc16c3e60153b
LuizGarbini <garbasneto@gmail.com>
2024-02-13 18:00:26 -0300

    update links images

--- landing-page-roland/script.js
+++ landing-page-roland/script.js
@@ -1,16 +1,20 @@
 function openModal(videoId) {
   let modal = document.getElementById('videoModal');
   let modalVideo = document.getElementById('modalVideo');
+  let modalBodyOverflow = document.querySelector('body')
   modalVideo.src =
     'https://www.youtube.com/embed/' + videoId + '?autoplay=1' + '&showinfo=0';
   modal.style.display = 'block';
+  modalBodyOverflow.style.overflow = 'hidden';
 }
 
 function closeModal() {
   let modal = document.getElementById('videoModal');
   let modalVideo = document.getElementById('modalVideo');
+  let modalBodyOverflow = document.querySelector('body')
   modal.style.display = 'none';
   modalVideo.src = '';
+  modalBodyOverflow.style.overflow = 'auto';
 }
 
 function openCustomModal(modalId) {
