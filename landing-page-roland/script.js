4e9a4e1776ff726ded76fea7a84cad6456ba0091
LuizGarbini <garbasneto@gmail.com>
2024-02-13 16:52:30 -0300

    page updates

--- landing-page-roland/script.js
+++ landing-page-roland/script.js
@@ -1,14 +1,31 @@
 function openModal(videoId) {
-  var modal = document.getElementById('videoModal');
-  var modalVideo = document.getElementById('modalVideo');
+  let modal = document.getElementById('videoModal');
+  let modalVideo = document.getElementById('modalVideo');
   modalVideo.src =
     'https://www.youtube.com/embed/' + videoId + '?autoplay=1' + '&showinfo=0';
   modal.style.display = 'block';
 }
 
 function closeModal() {
-  var modal = document.getElementById('videoModal');
-  var modalVideo = document.getElementById('modalVideo');
+  let modal = document.getElementById('videoModal');
+  let modalVideo = document.getElementById('modalVideo');
   modal.style.display = 'none';
   modalVideo.src = '';
+}
+
+function openCustomModal(modalId) {
+  let modal = document.getElementById(modalId);
+  let modalBodyOverflow = document.querySelector('body')
+  modal.style.display = 'block';
+  modalBodyOverflow.style.overflow = 'hidden';
+}
+
+function closeCustomModal(modalId, event) {
+  let modal = document.getElementById(modalId);
+  let closeButton = document.querySelector('#' + modalId + ' .close');
+  let modalBodyOverflow = document.querySelector('body')
+  if (event.target === modal || event.target === closeButton) {
+    modal.style.display = 'none';
+    modalBodyOverflow.style.overflow = 'auto';
+  }
 }
