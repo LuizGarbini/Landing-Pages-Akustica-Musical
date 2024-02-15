ad911fe91a03f8c202a9ecc099d41c911277faf4
LuizGarbini <garbasneto@gmail.com>
2024-02-15 19:13:20 -0300

    updates

--- landing-page-baterias-digitais/script.js
+++ landing-page-baterias-digitais/script.js
@@ -92,3 +92,20 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+function openCustomModal(modalId) {
+  let modal = document.getElementById(modalId);
+  let modalBodyOverflow = document.querySelector('body');
+  modal.style.display = 'block';
+  modalBodyOverflow.style.overflow = 'hidden';
+}
+
+function closeCustomModal(modalId, event) {
+  let modal = document.getElementById(modalId);
+  let closeButton = document.querySelector('#' + modalId + ' .close');
+  let modalBodyOverflow = document.querySelector('body');
+  if (event.target === modal || event.target === closeButton) {
+    modal.style.display = 'none';
+    modalBodyOverflow.style.overflow = 'auto';
+  }
+}
